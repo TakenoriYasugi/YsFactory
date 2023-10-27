10b08748218258b2ef9ef3230622cf412cef74a6
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-10-27 11:20:43 +0900

    編集、公開テスト

--- ys-factory/src/App.tsx
+++ ys-factory/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Context from './Context';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Context subtitle='This page is Running Test'/>
       </header>
     </div>
   );
---
+++ ys-factory/src/Context.tsx
@@ -0,0 +1,10 @@
+const Context = ({subtitle} : {subtitle: string}) => {
+    return (
+        <>
+        <h1>Hello World</h1>
+        <h2>{subtitle}</h2>
+        </>
+    );
+}
+
+export default Context;
