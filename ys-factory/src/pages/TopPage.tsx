3a4cfcddbf201f02e370eb15fff56d949f959502
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-10-31 04:04:20 +0900

    スタイル調整中

--- ys-factory/src/pages/TopPage.tsx
+++ ys-factory/src/pages/TopPage.tsx
@@ -4,22 +4,19 @@
  */
 
 import { Container } from '@mui/material';
-import mainLogo from '../images/mainlogo.webp' 
+import mainLogo from '../images/mainlogo.webp' ;
+import '../css/TopPageStyle.css';
 
 const TopPage = () => {
-
-    // ロゴを枠内に収めるためのスタイル
-    const logoStyle = {
-        maxWidth: "100%",
-        height: "auto",
-        display: "block"
-    }
 
     return (
         <>
             <Container>
-                <img src={mainLogo} style={logoStyle}/>
+                <img className='logo' src={mainLogo} />
+                <h1 className='headerText'>Y's Factoryへようこそ</h1>
             </Container>
+
+           
         </>
     );
 }
