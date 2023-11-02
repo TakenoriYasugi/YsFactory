1a7d9b3b4ef6c2521d97ac90d360f1750c1e0952
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-02 15:27:37 +0900

    デフォルトCSSのリセットを追加。

--- ys-factory/src/App.tsx
+++ ys-factory/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Context from './Context';
-import { Typography } from '@mui/material';
+import { CssBaseline, Typography } from '@mui/material';
 import DrawerAppBar from './uiparts/DrawerAppBar';
 import TopPage from './pages/TopPage';
 
 function App() {
   return (
     <>
+      <CssBaseline/>
       <DrawerAppBar/>
       <TopPage/>
     </>
