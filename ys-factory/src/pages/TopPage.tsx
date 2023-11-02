914f41c41b17044bc18a552cb52a464a076a7b8e
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-02 17:48:00 +0900

    トップページに雛形作成

--- ys-factory/src/pages/TopPage.tsx
+++ ys-factory/src/pages/TopPage.tsx
@@ -3,7 +3,7 @@
  * サイトのトップページに表示するコンテンツ
  */
 
-import { Container } from '@mui/material';
+import { Box, Container, Link, Typography } from '@mui/material';
 import headerImage from '../images/header.webp' ;
 import '../css/TopPageStyle.css';
 
@@ -13,6 +13,29 @@ const TopPage = () => {
         <>
             <img className='logo' src={headerImage} />
            
+           <Box component='main' sx={{p: 3}}>
+                <Box sx={{p: 2}}>
+                    <Typography variant='h5'>Y's Factoryへようこそ</Typography>
+                    <Typography>本サイトは安木岳規が運営するポートフォリオサイトです。</Typography>
+                </Box>
+                <Box sx={{p: 2}}>
+                    <Typography variant='h6'>プロフィール</Typography>
+                </Box>
+
+                <Box sx={{p: 2}}>
+                    <Typography variant='h6'>略歴</Typography>
+                </Box>
+
+                <Box sx={{p: 2}}>
+                    <Typography variant='h6'>スキルセット</Typography>
+                </Box>
+
+                <Box sx={{p: 2}}>
+                    <Typography variant='h6'>Git Hub</Typography>
+                    <Link href="https://github.com/TakenoriYasugi/">TakenoriYasugi</Link>
+                </Box>
+           </Box>
+
         </>
     );
 }
