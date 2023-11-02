91088597e135fc7cc49af5bb35800ae797367b83
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-02 15:41:36 +0900

    テーマカラーを変更

--- ys-factory/src/App.tsx
+++ ys-factory/src/App.tsx
@@ -2,16 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Context from './Context';
-import { CssBaseline, Typography } from '@mui/material';
+import { CssBaseline, ThemeProvider, Typography, createTheme } from '@mui/material';
 import DrawerAppBar from './uiparts/DrawerAppBar';
 import TopPage from './pages/TopPage';
+import { createJSDocTemplateTag } from 'typescript';
 
 function App() {
+  
+  // アプリ全体のテーマを設定
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#F9831B'
+      }
+    }
+  });
+
   return (
     <>
       <CssBaseline/>
-      <DrawerAppBar/>
-      <TopPage/>
+      <ThemeProvider theme={theme}>
+        <DrawerAppBar/>
+        <TopPage/>
+      </ThemeProvider>
     </>
   );
 }
