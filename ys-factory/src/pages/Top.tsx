c270e79ed3e58cd8c4f0ef5477670ce83110c473
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-07 18:45:44 +0900

    カードの仮配置

--- ys-factory/src/pages/Top.tsx
+++ ys-factory/src/pages/Top.tsx
@@ -3,7 +3,7 @@
  * サイトのトップページに表示するコンテンツ
  */
 
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Card, CardActionArea, CardContent, Container, Divider, Grid, Typography } from '@mui/material';
 import headerImage from '../images/header.webp' ;
 import '../css/TopStyle.css';
 import { Link } from 'react-router-dom';
@@ -21,6 +21,40 @@ const Top = () => {
                     <Typography variant='h5'>Y's Factoryへようこそ</Typography>
                     <Typography>本サイトは安木岳規が運営するポートフォリオサイトです。</Typography>
                 </Box>
+
+                <Grid container display={{sm: 'flex', xs: 'none'}}>
+                    <Grid item xs={6} sx={{p: 4}}>
+                        <Card sx={{p: 2}}>
+                            <CardActionArea href="/about">
+                                <CardContent>
+                                    <Typography fontSize={'medium'}>
+                                        About
+                                    </Typography>
+                                    <Divider/>
+                                    <Typography fontSize={'small'}>
+                                        自己紹介
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={6} sx={{p: 4}}>
+                        <Card sx={{p: 2}}>
+                            <CardActionArea href='/works'>
+                                <CardContent>
+                                    <Typography fontSize={'medium'}>
+                                        Works
+                                    </Typography>
+                                    <Divider/>
+                                    <Typography fontSize={'small'}>
+                                        開発したプロダクトの紹介
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                </Grid>
            </Box>
 
         </>
