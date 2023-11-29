a08e1db922ce7b6c1dde532e3f92f9a983062ae5
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-29 17:09:08 +0900

    レスポンシブ対応

--- ys-factory/src/uiparts/ImageSwiper.tsx
+++ ys-factory/src/uiparts/ImageSwiper.tsx
@@ -1,50 +1,35 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
+import { Box, Container, useTheme, useMediaQuery } from '@mui/material';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 
-// import required modules
-import { EffectCoverflow, Navigation, Pagination } from 'swiper/modules';
-
+// Import your images
 import echonorLogo from "../images/echonor/echonor_logo.png";
-import home from "../images/echonor/home.png"
-import menu from "../images/echonor/menu.png"
-import alert from "../images/echonor/alert.png"
-import bookmarks from "../images/echonor/bookmarks.png"
-import post from "../images/echonor/post.png"
-
-import { Box, Container } from '@mui/material';
-import { ReactNode } from 'react';
+import home from "../images/echonor/home.png";
+import menu from "../images/echonor/menu.png";
+import alert from "../images/echonor/alert.png";
+import bookmarks from "../images/echonor/bookmarks.png";
+import post from "../images/echonor/post.png";
 
 const ImageSwiper = () => {
-    const imageSize = "30%";
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+    const imageSize = isMobile ? "80%" : "30%";
     const imageStyle = {
         width: imageSize,
         height: imageSize,
         display: 'block',
         marginLeft: 'auto',
-        marginRight: 'auto'};
+        marginRight: 'auto'
+    };
 
-    const images: string[] = [
-        echonorLogo,
-        home,
-        post,
-        menu,
-        alert,
-        bookmarks,
-    ]
+    const images = [echonorLogo, home, post, menu, alert, bookmarks];
 
     return (
-        <Box sx={{
-            width: '100%', 
-            height: 'auto', 
-            maxWidth: '100%', 
-            display: 'block', 
-            marginLeft: 'auto', 
-            marginRight: 'auto' 
-          }}>
+        <Box sx={{ width: '100%', height: 'auto', display: 'block', margin: 'auto' }}>
             <Swiper
                 effect={'coverflow'}
                 grabCursor={true}
@@ -59,20 +44,18 @@ const ImageSwiper = () => {
                 }}
                 pagination={true}
                 modules={[EffectCoverflow, Pagination, Navigation]}
-                className="mySwiper"
                 navigation={true}
             >
-                <Container sx={{display: 'flex', justifyContent: "center", textAlign: "center"}}>
-                    {images.map((image) => (
-                        <SwiperSlide>
-                            <img src={image} style={imageStyle} />
+                <Container sx={{display: 'flex', justifyContent: "center"}}>
+                    {images.map((image, index) => (
+                        <SwiperSlide key={index}>
+                            <img src={image} style={imageStyle} alt={`Slide ${index}`} />
                         </SwiperSlide>
-                    ))};
+                    ))}
                 </Container>
-                
             </Swiper>
         </Box>
-    )
+    );
 }
 
 export default ImageSwiper;
